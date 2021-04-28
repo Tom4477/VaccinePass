f4bdecf5cf81d1d8310167d48975a3ff7bdaf8c3
Gogs <gogs@fake.local>
2021-04-28 22:28:54 +0800

    implements vaccine and vaccineStock manage class

---
+++ src/StockControl/Vaccine.java
@@ -0,0 +1,57 @@
+package StockControl;
+
+public class Vaccine {
+    private String name;
+    private String manufacture;
+    private int stage;
+    private String uniqueNo;
+    private String instruct;
+
+    public Vaccine(String name, String manufacture, int stage) {
+        this.name = name;
+        this.manufacture = manufacture;
+        this.stage = stage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacture() {
+        return manufacture;
+    }
+
+    public void setManufacture(String manufacture) {
+        this.manufacture = manufacture;
+    }
+
+    public int getStage() {
+        return stage;
+    }
+
+    public void setStage(int stage) {
+        this.stage = stage;
+    }
+
+    public String getInstruct() {
+        return instruct;
+    }
+
+    public void setInstruct(String instruct) {
+        this.instruct = instruct;
+    }
+
+    @Override
+    public String toString() {
+        return "VaccineInfo{" +
+                "name='" + name + '\'' +
+                ", manufacture='" + manufacture + '\'' +
+                ", stage=" + stage +
+                ", instruct='" + instruct + '\'' +
+                '}';
+    }
+}
--- src/StockControl/VaccineStock.java
+++ src/StockControl/VaccineStock.java
@@ -1,5 +1,73 @@
 package StockControl;
 
+import java.util.ArrayList;
+
 public class VaccineStock {
+
+    ArrayList<Vaccine> vaccines;
+
+    public VaccineStock()
+    {
+       vaccines=new ArrayList<>();
+    }
+
+    public void addVaccines(String vname,String vmanu,int vstage)
+    {
+        Vaccine instance=new Vaccine(vname,vmanu,vstage);
+        vaccines.add(instance);
+    }
+
+    public Vaccine consumeVaccines(String vname,String vmanu,int vstage)
+    {
+        Vaccine instance=new Vaccine(vname,vmanu,vstage);
+        if (vaccines.contains(instance))
+        {
+            vaccines.remove(instance);
+        }
+        return instance;
+    }
+
+    public Boolean queryVaccines(String vname,String vmanu,int vstage)
+    {
+        Vaccine instance=new Vaccine(vname,vmanu,vstage);
+        if (vaccines.contains(instance))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void ListAllVaccine()
+    {
+        int len=vaccines.size();
+        System.out.println("+++++++++++All Vaccine List+++++++++++");
+        for(int i=0;i<len;i++)
+        {
+            System.out.println("+++++++++++New  Vaccine+++++++++++");
+            Vaccine tmp=vaccines.get(i);
+            System.out.println(tmp.toString());
+        }
+    }
+    public int getNumberVacc()
+    {
+        return vaccines.size();
+    }
+
+    public int countByVnameStage(String  vname,int stage)
+    {
+        int num=0;
+        int len=vaccines.size();
+        for(int i=0;i<len;i++)
+        {
+            Vaccine tmp=vaccines.get(i);
+           if((vname.equalsIgnoreCase(tmp.getName()))&&(tmp.getStage()==stage))
+           {
+               num++;
+           }
+
+        }
+
+        return num;
+    }
 
 }
