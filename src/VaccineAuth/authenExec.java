5c13a67f754578750a342acab018aa94db05017d
Gogs <gogs@fake.local>
2021-05-03 23:06:54 +0800

    implements authentication logic

--- src/VaccineAuth/authenExec.java
+++ src/VaccineAuth/authenExec.java
@@ -1,11 +1,26 @@
 package VaccineAuth;
 
+import AuthenID.People;
+
+import java.util.Scanner;
+
 public class authenExec {
 
-    public void daemon()
-    {
+    public void daemon() {
+        Scanner scanner = new Scanner(System.in);
+        InonculateAuth inonculateAuth = new InonculateAuth();
+
         System.out.println("Inonculate Authentication");
-        System.out.println("please  input your name");
+        System.out.println("please  input your name：[Press Enter to contnue]");
+        String userName = scanner.nextLine();
+        System.out.println("please  input your ID  Number ：[Press Enter to contnue]");
+        String idNum = scanner.nextLine();
+        System.out.println("please  input your Gender (male or female)：[Press Enter to contnue]");
+        String gd = scanner.nextLine();
+
+        People newInstance = new People(userName, idNum, gd.charAt(0));
+
+        inonculateAuth.render(newInstance);
 
     }
 
