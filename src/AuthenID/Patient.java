28f2292cd03c30ace9d8519bd70f39d0b20853a1
Tom4477 <79796227+Tom4477@users.noreply.github.com>
2021-05-08 03:28:40 +0800

    Update Patient.java

--- src/AuthenID/Patient.java
+++ src/AuthenID/Patient.java
@@ -53,7 +53,7 @@ public class Patient {
     public void setInoculateRecord(HashMap<Vaccine, String> inoculateRecord) {
         InoculateRecord = inoculateRecord;
     }
-小于号 什么意思
+小于号 什么意思 hashmap 建key  value 对立起来；声明hashmap的对象/实例
     public void upsertInoculateRecord(Vaccine vaccine, String timeStamp) {
         if(InoculateRecord==null) {
             InoculateRecord = new HashMap<>();
@@ -67,11 +67,13 @@ public class Patient {
     }
 
     @Override
+   继承object，所有类都是o的子类patient也是子类 
     public String toString() {
         return personInfo.getName();
     }
-tostring 为什么
-    public String showInoculateInfo()
+tostring 为什么 对每个java类操作，是个java类的操作，转换为字符串
+   
+   public String showInoculateInfo()
     {
         if(InoculateRecord!=null)
         {
@@ -79,7 +81,9 @@ tostring 为什么
         }
 
         return "";
-解释一下这段代码
-return的这个是什么
+！什么意思 非的意思 is not的意思 反的意思
+   解释一下这段代码 显示疫苗信息
+return的这个是什么  deflult操作 默认操作；没有被if被过滤的， 输出了什么都没有 空字符串
+   为什么用这个？查出来就。。。查不出来就。。。
     }
 }
