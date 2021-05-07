4996ebd8b1438ccb66f3d0b39403c6c27e5cd387
Tom4477 <79796227+Tom4477@users.noreply.github.com>
2021-05-08 02:43:35 +0800

    Update patientManage.java

--- src/AuthenID/patientManage.java
+++ src/AuthenID/patientManage.java
@@ -6,9 +6,11 @@ import InventoryManagement.VaccineFactory;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+为什么是这样，util什么意思
 
 import static Storage.instanceStore.wait_queue;
 import static Storage.instanceStore.render_queue;
+为什么是这里
 
 public class patientManage {
     VaccineFactory vaccineFactory;
@@ -25,6 +27,7 @@ public class patientManage {
        Patient tmp=new Patient(person);
        wait_queue.add(tmp);
     }
+tmp是什么 为什么这样加
 
     public void inocute(Patient patient, Vaccine vaccine)
     {
@@ -34,6 +37,7 @@ public class patientManage {
         render_queue.add(patient);
         System.out.println("[Message from CDC] "+patient.getPersonInfo().getName()+" Inoculate successful!");
     }
+没有接种成功是怎样 stamp什么意思
     public String genNowDateStamp()
     {
         SimpleDateFormat simpleDateFormat=new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");
